ab2c9f20bfebc35ea1acc3f43ffa66b256ae4341
Tomas Tauber <tomtau@hku.hk>
2014-03-07 13:35:08 +0800

    created a test case for int type application

--- compiler/Main.hs
+++ compiler/Main.hs
@@ -44,8 +44,13 @@ program1 =
     )
   )
 
+-- should infer (forall (x0 : int) . int)
+intapp = FTApp idF PFInt 
+
+
+
 compiled1 = "abstract class Closure\n{\n  Object x;\n  Object out;\n  abstract void apply ()\n  ;\n}\nclass MyClosure extends Closure\n{\n  void apply ()\n  {\n    Closure x1 = new Closure()\n                 {\n                   Closure x2 = this;\n                   void apply ()\n                   {\n                     out = x2.x;\n                   }\n                 };\n    out = x1;\n  }\n}"
-compiled2 = "abstract class Closure\n{\n  Object x;\n  Object out;\n  abstract void apply ()\n  ;\n}\nclass MyClosure extends Closure\n{\n  void apply ()\n  {\n    Closure x2 = new Closure()\n                 {\n                   Closure x3 = this;\n                   {\n                     out = new Closure()\n                           {\n                             Closure x5 = this;\n                             void apply ()\n                             {\n                               Closure x6 = (Closure) x3.x;\n                               x6.x = x5.x;\n                               x6.apply();\n                               out = x6.out;\n                             }\n                           };\n                   }\n                   void apply ()\n                   {\n                     ;\n                   }\n                 };\n    Closure x3 = new Closure()\n                 {\n                   Closure x4 = this;\n                   void apply ()\n                   {\n                     out = x4.x;\n                   }\n                 };\n    Closure x1 = (Closure) x2;\n    x1.x = x3;\n    out = x1.out;\n  }\n}"
+compiled2 = "abstract class Closure\n{\n  Object x;\n  Object out;\n  abstract void apply ()\n  ;\n}\nclass MyClosure extends Closure\n{\n  void apply ()\n  {\n    Closure x2 = new Closure()\n                 {\n                   Closure x3 = this;\n                   {\n                     out = new Closure()\n                           {\n                             Closure x5 = this;\n                             void apply ()\n                             {\n                               Closure x6 = (Closure) x3.x;\n                               x6.x = x5.x;\n                               x6.apply();\n                               out = x6.out;\n                             }\n                           };\n                   }\n                   void apply ()\n                   {\n                   }\n                 };\n    Closure x3 = new Closure()\n                 {\n                   Closure x4 = this;\n                   void apply ()\n                   {\n                     out = x4.x;\n                   }\n                 };\n    Closure x1 = (Closure) x2;\n    x1.x = x3;\n    out = x1.out;\n  }\n}"
 compiled3 = "abstract class Closure\n{\n  Object x;\n  Object out;\n  abstract void apply ()\n  ;\n}\nclass MyClosure extends Closure\n{\n  void apply ()\n  {\n    Closure x1 = new Closure()\n                 {\n                   Closure x2 = this;\n                   void apply ()\n                   {\n                     Closure x5 = new Closure()\n                                  {\n                                    Closure x6 = this;\n                                    void apply ()\n                                    {\n                                      out = x6.x;\n                                    }\n                                  };\n                     Closure x3 = (Closure) x5;\n                     x3.x = x2.x;\n                     x3.apply();\n                     out = x3.out;\n                   }\n                 };\n    out = x1;\n  }\n}"
 
 test1 = TestCase $ assertEqual 
@@ -57,4 +62,7 @@ test2 = TestCase $ assertEqual
 test3 = TestCase $ assertEqual 
   "Should compile idF3" compiled3 ( let (cu,t) = createCU $ compile idF3 in (prettyPrint cu) )
 
-main = runTestTT $ TestList [test1, test2, test3]
+test4 = TestCase $ assertEqual
+  "Should infeer type of intapp" "(forall (x0 : Int) . Int)" ( let (cu, t) = createCU $ compile intapp in (gshowPCTyp3 t 0) )
+
+main = runTestTT $ TestList [test1, test2, test3, test4]
