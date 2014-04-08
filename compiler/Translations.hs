3d2cad0d0148564447ba87398dca9e92ba4e62a6
Tomas Tauber <tomtau@hku.hk>
2014-04-08 14:43:42 +0800

    Using ReaderMonad for cast substitution

--- compiler/Translations.hs
+++ compiler/Translations.hs
@@ -1,30 +1,45 @@
+{-# OPTIONS -XFlexibleContexts #-}
+
 module Translations where 
 
 import Mixins
-import TransCFJava
+import ApplyTransCFJava
 import StackTransCFJava
-import UberNaiveTransCFJava
+import BaseTransCFJava
+import SubstIntVarTransCFJava
+
+import Control.Monad.State
+import Control.Monad.Writer
+import Control.Monad.Reader
+import qualified Data.Map as Map
+import qualified Language.Java.Syntax as J
 
 -- Naive translation
 
-naive :: Translate
+naive :: (MonadState Int m) => Translate m
 naive = new trans
 
+-- apply() distinction 
+transMixA :: (MonadState Int m, MonadWriter Bool m) => Open (ApplyOptTranslate m)
+transMixA this = NT (trans (toT this))
+
+applyopt :: (MonadState Int m, MonadWriter Bool m) => ApplyOptTranslate m
+applyopt = new (transApply . transMixA)
+
+transMixS :: (MonadState Int m, MonadReader (Map.Map J.Exp J.Exp) m) => Open (SubstIntVarTranslate m)
+transMixS this = VNT (trans (toTST this)) (translateSubst this) (translateScopeSubst this)
+
+substopt :: (MonadState Int m, MonadReader (Map.Map J.Exp J.Exp) m) => SubstIntVarTranslate m
+substopt = new (transNewVar . transMixS)
+--
 -- Stack-based translation 
 
 -- Adaptor mixin for trans
 
-transMix :: Open TranslateStack 
-transMix this = TS (trans (toT this)) (translateScheduleM this)
+transMix :: (MonadState Int m, MonadWriter Bool m) => Open (TranslateStack m)
+transMix this = TS (applyopt) (translateScheduleM this)
              
 -- mixing in the new translation
 
-stack :: TranslateStack
+stack :: (MonadState Int m, MonadWriter Bool m) => TranslateStack m
 stack = new (transS . transMix)
-
--- adaptor for the translation without apply() distinction - not sure whether really needed
-transMixN :: Open NaiveTranslate 
-transMixN this = NT (trans (toTr this))
-
-ubernaive :: NaiveTranslate
-ubernaive = new (transUN . transMixN)
