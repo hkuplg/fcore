e7d72d72f11eae2bce5fbc4fce7549ab41c87461
zhiyuanshi <zhiyuanshi@server.fake>
2014-06-27 16:38:38 +0800

    Clean up

--- compiler/Main.hs
+++ compiler/Main.hs
@@ -1,33 +1,26 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# OPTIONS -XMultiParamTypeClasses -XRankNTypes -XFlexibleContexts -XTypeOperators  -XOverlappingInstances #-}
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, RankNTypes, FlexibleContexts, TypeOperators, OverlappingInstances #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 module Main where
 
 import Data.List
 import qualified Data.Char as Char (toUpper)
 import Data.Map
-import qualified Data.Set as Set
 import Language.Java.Pretty
 import Language.Java.Syntax as J
 import Prelude hiding (const)
-import System.Directory
 import qualified System.Environment       (getArgs)
-import System.Exit
 import System.FilePath
-import System.Process
 
 -- import HMParser         (readHM)
 -- import qualified HM
-import ApplyTransCFJava
+-- import ApplyTransCFJava
 import BaseTransCFJava
-import BaseTransCFJava (createCU)
 import ClosureF
 import Inheritance
 import MonadLib
 import SystemF.Syntax
 import Translations
 import qualified SystemF.Parser
-import qualified TestSuite as TS
 
 type M1 = StateT (Map String Int) (State Int)
 
@@ -45,12 +38,12 @@ sopt = applyopt
 -}
 
 translate ::  PCExp Int (Var, PCTyp Int) -> MAOpt ([BlockStmt], Exp, PCTyp Int)
-translate e = translateM (up sopt) e
+translate = translateM (up sopt) 
 
 compile ::  PFExp Int (Var, PCTyp Int) -> (Block, Exp, PCTyp Int)
 compile e = 
-  case fst $ runWriter $ (evalStateT (evalStateT (translate (fexp2cexp e)) 0) empty) of
-      (ss,exp,t) -> (J.Block ss,exp, t)
+  case fst $ runWriter $ evalStateT (evalStateT (translate (fexp2cexp e)) 0) empty of
+      (ss,e1,t) -> (J.Block ss,e1, t)
 
 {-
 type MAOpt = StateT Int (StateT (Map J.Exp Int) (Reader (Set.Set Int))) 
@@ -81,18 +74,16 @@ translate e = translateM (to sopt) e
 prettyJ :: Pretty a => a -> IO ()
 prettyJ = putStrLn . prettyPrint
 
+compilePretty :: PFExp Int (Var, PCTyp Int) -> IO ()
+compilePretty e = let (b,e1,t) = compile e in (prettyJ b >> prettyJ e1 >> print t)
 
-
-
-compilePretty e = let (b,exp,t) = compile e in (prettyJ b >> prettyJ exp >> putStrLn (show t))
-
-compileCU className e (Just nameStr) = let (cu,t) = (createCU className (compile e) (Just nameStr)) in (prettyJ cu >> putStrLn (show t))
-
-compileCU className e Nothing = let (cu,t) = (createCU className (compile e) Nothing) in (prettyJ cu >> putStrLn (show t))
+compileCU :: String -> PFExp Int (Var, PCTyp Int) -> Maybe String -> IO ()
+compileCU className e (Just nameStr) = let (cu,t) = createCU className (compile e) (Just nameStr) in (prettyJ cu >> print t)
+compileCU className e Nothing = let (cu,t) = createCU className (compile e) Nothing in (prettyJ cu >> print t)
 
 -- SystemF to Java
 sf2java :: String -> String -> String
-sf2java className src = let (cu, _) = (createCU className (compile (SystemF.Parser.reader src)) Nothing) in prettyPrint cu
+sf2java className src = let (cu, _) = createCU className (compile (SystemF.Parser.reader src)) Nothing in prettyPrint cu
 
 -- `compilesf2java srcPath outputPath` loads a SystemF file at `srcPath`,
 -- and writes the compiled Java code to `outputPath`.
@@ -106,8 +97,8 @@ compilesf2java srcPath outputPath = do
 
 inferOutputPath :: String -> String
 inferOutputPath srcPath = directory </> className ++ ".java"
-    where directory  = takeDirectory srcPath
-          className  = capitalize $ takeBaseName srcPath
+    where directory = takeDirectory srcPath
+          className = capitalize $ takeBaseName srcPath
 
 inferClassName :: String -> String
 inferClassName outputPath = capitalize $ takeBaseName outputPath
