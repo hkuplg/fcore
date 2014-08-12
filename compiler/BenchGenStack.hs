88401c4897e8c51ea39efa3982c85c606022108e
Jerry Zhang <zzrzczyyx2@163.com>
2014-08-12 23:23:31 +0800

    add file benchgenstack

---
+++ compiler/BenchGenStack.hs
@@ -0,0 +1,139 @@
+{-# OPTIONS -XRankNTypes -XFlexibleInstances -XFlexibleContexts -XTypeOperators -XMultiParamTypeClasses  -XScopedTypeVariables -XKindSignatures -XUndecidableInstances -XOverlappingInstances #-}
+
+module BenchGenStack where
+
+import Prelude hiding (init, last)
+
+import qualified Language.Java.Syntax as J
+import Inheritance
+--import qualified Data.Map as Map
+--import qualified Data.Set as Set
+import ClosureF
+import BaseTransCFJava
+import StringPrefixes
+import MonadLib
+
+import Language.Java.Pretty
+import Text.PrettyPrint.Leijen
+
+data BenchGenTranslateStack m = TBS {
+  toTBS :: Translate m -- supertype is a subtype of Translate (later on at least)
+}
+
+instance (:<) (BenchGenTranslateStack m) (Translate m) where
+   up = up . toTBS
+
+instance (:<) (BenchGenTranslateStack m) (BenchGenTranslateStack m) where -- reflexivity
+  up = id
+
+
+-- nats = 1 : map (+1) nats
+
+testfuncArgType paraType= map (\x -> J.FormalParam [] (J.PrimType J.IntT) False (J.VarId (J.Ident $ "x" ++ show x))) paraType
+
+-- Closure c0 = (Closure) apply();
+closureInit id = (J.LocalVars []  closureType--type
+	 [J.VarDecl (J.VarId (J.Ident id)) -- name
+	 	(Just $ J.InitExp $ (J.Cast closureType) (J.MethodInv $ J.MethodCall (J.Name [(J.Ident "apply")]) []))]) -- init
+
+fieldAcc classId fieldId = (J.PrimaryFieldAccess (J.ExpName (J.Name [(J.Ident classId)])) (J.Ident fieldId))
+
+-- Closure c1 = (Closure) c0.out;
+closurePass idl idr = (J.LocalVars [] closureType --type
+	 [J.VarDecl (J.VarId (J.Ident idl)) -- name
+	 	(Just $ J.InitExp $ (J.Cast closureType) (J.FieldAccess $ (fieldAcc idr "out")))]) -- init
+
+-- c1.x = x;
+paraAssign classId paraId = (J.BlockStmt $ J.ExpStmt $ J.Assign (J.FieldLhs $ (fieldAcc classId "x")) (J.EqualA) (J.ExpName $ J.Name [(J.Ident paraId)]))
+
+-- c1.apply();
+invokeApply classId = (J.BlockStmt $ J.ExpStmt $ J.MethodInv $ (J.PrimaryMethodCall (J.ExpName $ (J.Name [J.Ident classId])) [] (J.Ident "apply") []))
+
+-- return (Integer) c2.out;
+retRes returnType classId = (J.BlockStmt (J.Return $ Just (J.Cast (J.RefType $ (refType returnType)) (J.FieldAccess (fieldAcc classId "out")))))
+
+-- here
+testfuncBody paraType = 
+	case paraType of
+		[] -> []
+		x : [] -> [ closureInit c0, paraAssign c0 x0, invokeApply c0, retRes "Integer" c0] 
+				where
+					c0 = "c" ++ show x
+					x0 = "x" ++ show x
+					
+		x : y : [] ->
+			[ closureInit c0, 
+			  paraAssign  c0 x0 , 
+			  invokeApply c0, 
+			  closurePass c1 c0, 
+			  paraAssign c1 x1, 
+			  invokeApply c1, 
+			  retRes "Integer" c1]
+			where
+				c0 = "c" ++ show x
+				x0 = "x" ++ show x
+				c1 = "c" ++ show y
+				x1 = "x" ++ show y
+		_ -> []
+
+getClassDecl className bs ass paraType testfuncBody returnType mainbodyDef = J.ClassTypeDecl (J.ClassDecl [J.Public] (J.Ident className) [] (Nothing) []
+	(J.ClassBody [app [J.Static] body returnType "apply" [],
+	  app [J.Public, J.Static] (Just (J.Block (testfuncBody paraType))) returnType "test" (testfuncArgType paraType),
+      app [J.Public, J.Static] mainbodyDef Nothing "main" mainArgType]))
+    where
+        body = Just (J.Block (bs ++ ass))
+
+getParaType :: (PCTyp Int) -> [Int]
+getParaType tp = case tp of
+					CInt -> []
+					CTVar _ -> []
+					CForall a -> getScopeType a 0
+					_ -> []
+
+-- (Scope b t e) -> [Int]
+getScopeType (Kind f) n = 0 : (map (+1) (getScopeType (f n) (n)))
+getScopeType (Typ t f) n = 0 : (map (+1) (getScopeType (f ()) 0))
+getScopeType _ _= []
+
+benchmarkPackage name = Just (J.PackageDecl (J.Name [(J.Ident name)]))
+
+createCUB this compDef = cu where
+   cu = J.CompilationUnit (benchmarkPackage "benchmark") [] ([closureClass this] ++ compDef)
+
+
+
+transBenchStack :: (MonadState Int m, selfType :< BenchGenTranslateStack m, selfType :< Translate m) => Mixin selfType (Translate m) (BenchGenTranslateStack m)
+transBenchStack this super = TBS {
+  toTBS = T { 
+
+  translateM = translateM super,
+
+  translateScopeM = translateScopeM super,
+
+  -- here, I guess, you will mainly do the changes: have a look at BaseTransCFJava (and StackTransCFJava) how it's done currently             
+  --createWrap = \name exp ->
+  --      do (bs,e,t) <- translateM super exp
+  --         let returnType = case t of CInt -> Just $ J.PrimType $ J.IntT
+  --                                    _ -> Just $ objType
+  --         let maybeCastedReturnExp = case t of CInt -> J.Cast boxedIntType e
+  --                                              _ -> J.Cast objType e
+  --         let paraType = getParaType t
+  --         let classDecl = BenchGenStack.getClassDecl name bs ([J.BlockStmt (J.Return $ Just maybeCastedReturnExp)]) paraType testfuncBody returnType mainbody
+  --         return (BenchGenStack.createCUB super [classDecl], t), 
+
+  
+  createWrap = createWrap super,
+
+  closureClass = closureClass super,
+
+  translateApply = translateApply super,
+
+  genApply = genApply super, 
+
+  genRes = genRes super,
+
+  getCvarAss = getCvarAss super
+
+   }
+}
+
