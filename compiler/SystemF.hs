5755cf394dc12e142db107d072bbe35f55c84400
zhiyuanshi <zhiyuanshi@server.fake>
2014-08-15 13:01:36 +0800

    Clean up

--- compiler/SystemF.hs
+++
@@ -1,7 +0,0 @@
-module Language.SystemF where
-
-import Language.SystemF.Syntax
-import Language.SystemF.Lexer
-import Language.SystemF.Parser
-import Language.SystemF.TypeCheck
-import Language.SystemF.Pretty
