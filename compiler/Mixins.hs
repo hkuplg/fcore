71d3b0bbbed789654dbcddc2aa65883160bc4f9b
zhiyuanshi <zhiyuanshi@server.fake>
2014-08-17 02:16:18 +0800

    Add basic mixin combinators

--- compiler/Mixins.hs
+++ compiler/Mixins.hs
@@ -5,3 +5,27 @@ type Open a = a -> a
 new :: Open a -> a
 new f = let r = f r in r
 
+-- Basic mixin combinators
+
+type Mixin s = s -> s -> s
+
+extends :: Mixin s -> Mixin s -> Mixin s
+f `extends` g = \super this -> f (g super this) this
+
+zero :: Mixin s
+zero super this = super
+
+mixin :: Mixin s -> s
+mixin f = let m = mixin f in f m m
+
+-- Examples
+
+data G = G { value :: Int, square :: Int } deriving (Show)
+
+g1, g2 :: Mixin G
+g1 super this = G { value = 7, square = value this * value this }
+g2 super this = super { value = 2 }
+
+m1, m2 :: G
+m1 = mixin g1
+m2 = mixin (g2 `extends` g1)
