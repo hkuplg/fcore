d1aa9e4ddacf69eeb4fb26bb7e7bc2534bf2d4f7
zhiyuanshi <zhiyuanshi@server.fake>
2014-08-18 22:52:35 +0800

    Add SpecHelper.discoverTestCases'

--- testsuite/SpecHelper.hs
+++ testsuite/SpecHelper.hs
@@ -1,4 +1,7 @@
-module SpecHelper (discoverTestCases) where
+module SpecHelper
+  (discoverTestCases
+  ,discoverTestCases'
+  ) where
 
 import System.Directory (getDirectoryContents)
 import System.FilePath  (dropExtension, (</>))
@@ -6,15 +9,31 @@ import System.FilePath  (dropExtension, (</>))
 import Control.Applicative ((<$>))
 import Control.Monad       (forM)
 
+import Data.Char (isSpace)
 import Data.List (isSuffixOf)
 
 type Name   = String
 type Source = String
-type TestCase = (Name, Source)
+type ExpectedOutput = String
 
-discoverTestCases :: FilePath -> IO [TestCase]
+discoverTestCases :: FilePath -> IO [(Name, Source)]
 discoverTestCases directory =
   do fileNames <- filter (isSuffixOf ".sf") <$> getDirectoryContents directory
      forM fileNames (\f ->
        do source <- readFile (directory </> f)
           return (dropExtension f, source))
+
+discoverTestCases' :: FilePath -> IO [(Name, Source, ExpectedOutput)]
+discoverTestCases' directory =
+  do fileNames <- filter (isSuffixOf ".sf") <$> getDirectoryContents directory
+     forM fileNames (\f ->
+       do source <- readFile (directory </> f)
+          let firstLine = takeWhile (/= '\n') source
+          case firstLine of
+            '-':'-':'>':_ -> return (dropExtension f, source, strip (drop 3 firstLine))
+            _             -> error (directory </> f ++ ": " ++
+                                    "The integration test file should start with '-->', \
+                                    \followed by the expected output"))
+
+strip :: String -> String
+strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace
