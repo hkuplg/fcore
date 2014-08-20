5b22de9608f3f2d9b6d2b0f96b96b902458a3513
Tomas Tauber <tomtau@hku.hk>
2014-08-21 03:08:09 +0800

    public fields/methods now in runtime

--- runtime/src/hk/hku/cs/f2j/Closure.java
+++ runtime/src/hk/hku/cs/f2j/Closure.java
@@ -2,9 +2,9 @@ package hk.hku.cs.f2j;
 
 public abstract class Closure
 {
-  Object x;
-  Object out;
-  abstract void apply ()
+  public Object x;
+  public Object out;
+  public abstract void apply ()
   ;
   public Closure clone () {
       return (Closure) ((Object) this.clone());
