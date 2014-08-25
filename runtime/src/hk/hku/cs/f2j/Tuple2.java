2da6c757c82a1da0dbbf1e5a1afc9a25c4971491
Jerry Zhang <zzrzczyyx2@163.com>
2014-08-25 23:53:12 +0800

    add Tuple2.java to runtime

---
+++ runtime/src/hk/hku/cs/f2j/Tuple2.java
@@ -0,0 +1,11 @@
+package hk.hku.cs.f2j;
+
+public class Tuple2{
+	public final Object fst;
+	public final Object snd;
+
+	public Tuple2(Object fst, Object snd){
+		this.fst = fst;
+		this.snd = snd;
+	}
+}
