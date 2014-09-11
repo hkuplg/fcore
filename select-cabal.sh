ea65be6e4ea2d1be64710eaa2996ed3273f6c6d4
Zhiyuan Shi <zhiyuan.shi@gmail.com>
2014-09-11 21:52:28 +0800

    Use the modern syntax of command expansion in bash: $(...)

--- select-cabal.sh
+++ select-cabal.sh
@@ -6,7 +6,7 @@ project_name="systemfcompiler"
 
 ghc --version
 
-if [[ `ghc --version` =~ 7.8 ]]; then
+if [[ $(ghc --version) =~ 7.8 ]]; then
   cp $project_name.cabal.ghc-7.8.3 $project_name.cabal
 else
   cp $project_name.cabal.ghc-7.6.3 $project_name.cabal
