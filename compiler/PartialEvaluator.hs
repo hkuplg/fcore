a078c9d1109a9efb0b58422fc6022ad6e4f1764b
Weixin Zhang <zhangweixinxd@gmail.com>
2014-09-22 18:27:42 +0800

    Existing problem: App e1 e2 not fully partially evaluated

--- compiler/PartialEvaluator.hs
+++ compiler/PartialEvaluator.hs
@@ -10,11 +10,15 @@ peval :: Expr t (Expr t e) -> Expr t e
 peval (Var x) = x
 peval (Lam t f) = Lam t (\x -> peval . f . Var $ x)
 peval (BLam f) = BLam (\t -> peval . f $ t)
-peval (App e1 e2) = -- not correct
+peval (App e1 e2) =
     case e1 of
-      (Lam t f) -> peval . f . peval $ e2
-      fix@(Fix _ _ _) -> peval fix
-      _ -> peval $ App (peval e1) (peval e2)
+      Lam t f -> peval . f . peval $ e2
+      _ -> App (peval e1) (peval e2)
+{-
+    -- type-error. f2 is of type "e -> Expr t e" while f is of type "Expr t e -> Expr t (Expr t e)"
+    case peval e1 of
+      Lam t f2 -> f2 $ peval e2
+-}
 peval (TApp e t) = TApp (peval e) t
 peval (Lit s) = Lit s
 peval (If e1 e2 e3) = 
@@ -23,16 +27,29 @@ peval (If e1 e2 e3) =
       Lit (S.Boolean False) -> peval e3
       _ -> If e1' (peval e2) (peval e3)
     where e1' = peval e1
-peval (PrimOp e1 op e2) = -- Not exhausitive
+peval (PrimOp e1 op e2) = -- TODO: op excludes (LShift RShift RRShift Xor CAnd COr)
     case (e1', e2') of
       (Lit (S.Integer a), Lit (S.Integer b)) ->
           case op of
+            -- arithmetic operations
             S.Arith J.Sub -> Lit . S.Integer $ a - b
+            S.Arith J.Add -> Lit . S.Integer $ a + b
+            S.Arith J.Mult -> Lit . S.Integer $ a * b
+            S.Arith J.Div -> Lit . S.Integer $ a `div` b
+            S.Arith J.Rem -> Lit . S.Integer $ a `rem` b
+            -- comparison operations
             S.Compare J.Equal -> Lit . S.Boolean $ a == b
+            S.Compare J.NotEq -> Lit . S.Boolean $ a /= b
+            S.Compare J.LThan -> Lit . S.Boolean $ a < b
+            S.Compare J.LThanE -> Lit . S.Boolean $ a <= b
+            S.Compare J.GThan -> Lit . S.Boolean $ a > b
+            S.Compare J.GThanE -> Lit . S.Boolean $ a >= b
             _ -> simplified
       (Lit (S.Boolean a), Lit (S.Boolean b)) ->
           case op of
+            -- logic operations
             S.Logic J.And -> Lit . S.Boolean $ a && b
+            S.Logic J.Or -> Lit . S.Boolean $ a || b
             _ -> simplified
       _ -> simplified
     where e1' = peval e1
@@ -50,32 +67,3 @@ peval (JMethod cnameOrE mname es cname) = JMethod (fmap peval cnameOrE) mname (m
 peval (JField cnameOrE fname cname) = JField (fmap peval cnameOrE) fname cname
 peval (Seq es) = Seq $ map peval es
 peval (Merge e1 e2) = Merge (peval e1) (peval e2)
-
-identity :: Expr t e
-identity = Lam javaInt (\x -> Var x)
-id_1_1 = peval (App identity one) `eq` one
-
-id_twice = Lam javaInt (\x -> App identity (Var x))
-id_twice_1_1 = peval (App id_twice one) `eq` one
-
-minus :: Expr t e
-minus = Lam javaInt (\x -> Lam javaInt (\y -> Var x `sub` Var y))
-
--- test nested App
-minus_1 = App minus one
-zero_minus_1 = App minus_1 zero
-minus_1_0_be_1 = peval (App (App minus one) zero) `eq` one
-
-app_lam_if = App (Lam javaInt (\x -> If ((Var x) `eq` one)
-                                 ((Var x) `sub` one)
-                                 (zero `sub` one)))
-          (zero `sub` one)
-app_lam_app = App (Lam javaInt (\x -> App (Lam javaInt (\y -> (Var x) `sub` (Var y))) zero))
-       one
-
-fix = Fix (\f n -> If (((one `sub` zero) `eq` zero))
-                   one
-                   (Var n `mult` (Var f `App` (Var n `sub` one))))
-      javaInt
-      (javaInt `Fun` javaInt)
-app_fix = App fix (Lit (S.Integer 10))
