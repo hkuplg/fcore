02d698cfa7a087f282cef93a469587c33803139f
Weixin Zhang <zhangweixinxd@gmail.com>
2014-09-23 16:44:43 +0800

    1. Refactor calc using mapExpr 2. Move mapExpr to OptiUtils.hs

--- compiler/PartialEvaluator.hs
+++ compiler/PartialEvaluator.hs
@@ -15,10 +15,6 @@ betaReduct (App e1 e2) =
 betaReduct e = mapExpr betaReduct e
 
 calc :: Expr t e -> Expr t e
-calc (Lam t f) = Lam t (\x -> calc . f $ x)
-calc (BLam f) = BLam (\t -> calc . f $ t)
-calc (App e1 e2) = App (calc e1) (calc e2)
-calc (TApp e t) = TApp (calc e) t
 calc (PrimOp e1 op e2) =
     case (e1', e2') of
       (Lit (S.Integer a), Lit (S.Integer b)) ->
@@ -47,48 +43,13 @@ calc (PrimOp e1 op e2) =
     where e1' = calc e1
           e2' = calc e2
           simplified = PrimOp e1' op e2'
-calc (Tuple es) = Tuple $ map calc es
-calc (Proj i e) = Proj i (calc e)
-calc (Fix f t1 t) = Fix (\e1 e2 -> calc $ f e1 e2) t1 t
-calc (LetRec sigs binds body) =
-    LetRec sigs
-           (\es -> map calc $ binds es)
-           (\es -> calc $ body es)
-calc (JNewObj cname es) = JNewObj cname (map calc es)
-calc (JMethod cnameOrE mname es cname) = JMethod (fmap calc cnameOrE) mname (map calc es) cname
-calc (JField cnameOrE fname cname) = JField (fmap calc cnameOrE) fname cname
-calc (Seq es) = Seq $ map calc es
-calc (Merge e1 e2) = Merge (calc e1) (calc e2)
 calc (If e1 e2 e3) =
-    case calc e1 of
+    case e1' of
       Lit (S.Boolean True) -> calc e2
       Lit (S.Boolean False) -> calc e3
-      _ -> If (calc e1) (calc e2) (calc e3)
-calc e = e
-
-mapExpr :: (Expr t e -> Expr t e) -> Expr t e -> Expr t e
-mapExpr f e =
-    case e of
-      Var x -> e
-      Lit l -> e
-      Lam t g -> Lam t (\x -> f . g $ x)
-      Fix g t1 t -> Fix (\e1 e2 -> f $ g e1 e2) t1 t
-      LetRec sigs binds body ->
-          LetRec sigs
-                 (\es -> map f $ binds es)
-                 (\es -> f $ body es)
-      BLam g -> BLam (\x -> f . g $ x)
-      App e1 e2 -> App (f e1) (f e2)
-      TApp e t -> TApp (f e) t
-      If e1 e2 e3 -> If (f e1) (f e2) (f e3)
-      PrimOp e1 op e2 -> PrimOp (f e1) op (f e2)
-      Tuple es -> Tuple $ map f es
-      Proj i e -> Proj i (f e)
-      JNewObj cname es -> JNewObj cname (map f es)
-      JMethod cnameOrE mname es cname -> JMethod (fmap f cnameOrE) mname (map f es) cname
-      JField cnameOrE fname cname -> JField (fmap f cnameOrE) fname cname
-      Seq es -> Seq $ map f es
-      Merge e1 e2 -> Merge (f e1) (f e2)
+      _ -> If e1' (calc e2) (calc e3)
+    where e1' = calc e1
+calc e = mapExpr calc e
 
 peval :: Expr t (Expr t e) -> Expr t e
 peval = calc . joinExpr . betaReduct
