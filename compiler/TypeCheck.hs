4a0aaa2bf34201f1cafda14c9d6ead25860dce89
Zhiyuan Shi <zhiyuan.shi@gmail.com>
2015-01-19 05:59:51 +0800

    Let `inferBind` check inferred type against annotated type

--- compiler/TypeCheck.hs
+++ compiler/TypeCheck.hs
@@ -518,16 +518,26 @@ inferAgainstAnyJClass expr
              (code (pretty expr) <+> text "has type" <+> code (pretty ty) <> comma <+>
               text "but is expected to be of some Java class")
 
--- | Check "f A1 ... An (x1:T1) ... (xn:Tn) = e"
+-- | Check "f A1 ... An (x1:T1) ... (xn:Tn) : t = e"
 inferBind :: ReaderBind -> Checker (Name, Type, CheckedExpr)
 inferBind bind
   = do bind' <- checkBindLHS bind
        (bindRhs', bindRhsTy) <- withLocalTVars (map (\a -> (a, (Star, TerminalType))) (bindTargs bind')) $
                                   do expandedBindArgs <- mapM (\(x,t) -> do { d <- getTypeContext; return (x,expandType d t) }) (bindArgs bind')
                                      withLocalVars expandedBindArgs (infer (bindRhs bind'))
-       return ( bindId bind'
-              , wrap Forall (bindTargs bind') (wrap Fun (map snd (bindArgs bind')) bindRhsTy)
-              , wrap BLam (bindTargs bind') (wrap Lam (bindArgs bind') bindRhs'))
+       case bindRhsAnnot bind' of
+         Nothing -> return ( bindId bind'
+                           , wrap Forall (bindTargs bind') (wrap Fun (map snd (bindArgs bind')) bindRhsTy)
+                           , wrap BLam (bindTargs bind') (wrap Lam (bindArgs bind') bindRhs'))
+         Just annot ->
+           withLocalTVars (map (\a -> (a, (Star, TerminalType))) (bindTargs bind')) $
+             do checkType annot
+                d <- getTypeContext
+                if alphaEq d annot bindRhsTy
+                   then return (bindId bind'
+                               , wrap Forall (bindTargs bind') (wrap Fun (map snd (bindArgs bind')) bindRhsTy)
+                               , wrap BLam (bindTargs bind') (wrap Lam (bindArgs bind') bindRhs'))
+                   else throwError (TypeMismatch (expandType d annot) bindRhsTy)
 
 -- | Check the LHS to the "=" sign of a bind, i.e., "f A1 ... An (x1:t1) ... (xn:tn)".
 -- First make sure the names of type params and those of value params are distinct, respectively.
