c78dfc568a4568257484d5b4a1c86a75d46d70de
Jeremy Bi <bixuanxbi@gmail.com>
2015-01-19 14:01:01 +0800

    Be prudent when calling clone (only in the var node)

--- compiler/BaseTransCFJava.hs
+++ compiler/BaseTransCFJava.hs
@@ -48,7 +48,7 @@ type TransType = ([J.BlockStmt], TransJavaExp, Type Int)
 data Translate m =
   T {translateM :: Expr Int (Var,Type Int) -> m TransType
     ,translateScopeM :: EScope Int (Var, Type Int) -> Maybe (Int,Type Int) -> m ([J.BlockStmt],TransJavaExp,TScope Int)
-    ,translateApply :: m TransType -> m TransType -> m TransType
+    ,translateApply :: Bool -> m TransType -> m TransType -> m TransType
     ,translateIf :: m TransType -> m TransType -> m TransType -> m TransType
     ,translateLet :: TransType -> ((Var, Type Int) -> Expr Int (Var, Type Int)) -> m TransType
     ,translateScopeTyp :: Int -> Int -> [J.BlockStmt] -> EScope Int (Var, Type Int) -> m ([J.BlockStmt],TransJavaExp,TScope Int) -> String -> m ([J.BlockStmt],TScope Int)
@@ -63,7 +63,7 @@ data Translate m =
     ,javaType :: Type Int -> m J.Type
     ,chooseCastBox :: Type Int -> m (String -> J.Exp -> J.BlockStmt, J.Type)
     ,stackMainBody :: Type Int -> m [J.BlockStmt]
-    ,genClosureVar :: Int -> TransJavaExp -> m J.Exp
+    ,genClosureVar :: Bool -> Int -> TransJavaExp -> m J.Exp
     ,createWrap :: String -> Expr Int (Var,Type Int) -> m (J.CompilationUnit,Type Int)}
 
 -- needed
@@ -271,7 +271,9 @@ trans self =
 -}
               App e1 e2 -> -- app e1 e2: e1 and e2 can't be in tail position, the whole inherits flag
                 do (n :: Int, _ :: Bool) <- ask
-                   translateApply this
+                   let flag = case e1 of Var _ -> True
+                                         _ -> False
+                   translateApply this flag
                                   (local (\(_ :: Int, _ :: Bool) -> (n+1, False)) $ translateM this e1)
                                   (local (\(_ :: Int, _ :: Bool) -> (0, False)) $ translateM this e2)
               -- InstanceCreation [TypeArgument] ClassType [Argument] (Maybe ClassBody)
@@ -392,11 +394,11 @@ trans self =
 
                    return (cvar ++ fstmt,var (localvarstr ++ show n),Type t (const t1))
        ,translateApply =
-          \m1 m2 ->
+          \flag m1 m2 ->
             do (s1, j1',Forall (Type _ g)) <- m1
                (s2,j2,_) <- m2
                let retTyp = g ()
-               j1 <- genClosureVar this (getArity retTyp) j1'
+               j1 <- genClosureVar this flag (getArity retTyp) j1'
                (s3,nje3) <- getS3 this j1 (unwrap j2) retTyp closureType
                return (s2 ++ s1 ++ s3,nje3,scope2ctyp retTyp)
        ,translateIf =
@@ -435,7 +437,7 @@ trans self =
                       ,t1)
        ,genApply = \f _ _ _ _ -> return [bStmt $ applyMethodCall f]
        ,genRes = const return
-       ,genClosureVar = \_ j1 ->  return (unwrap j1)
+       ,genClosureVar = \_ _ j1 ->  return (unwrap j1)
        ,javaType = \typ -> case typ of
                              (JClass c) -> return $ classTy c
                              (Forall _) -> do closureClass <- liftM2 (++) (getPrefix this) (return "Closure")
