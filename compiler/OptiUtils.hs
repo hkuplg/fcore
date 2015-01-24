2306390d7e1496d2d6539580dd15c58c2de3170b
Weixin Zhang <zhangweixinxd@gmail.com>
2015-01-24 19:37:14 +0800

    Refactor with hlint

--- compiler/OptiUtils.hs
+++ compiler/OptiUtils.hs
@@ -11,9 +11,9 @@ import Simplify (simplify)
 
 joinExpr :: Expr t (Expr t e) -> Expr t e
 joinExpr (Var _ x) = x
-joinExpr (Lam n t1 e1) = Lam n t1 (\ee -> joinExpr (e1 (Var n ee)))
+joinExpr (Lam n t1 e1) = Lam n t1 (joinExpr . e1 . Var n )
 joinExpr (App e1 e2) = App (joinExpr e1) (joinExpr e2)
-joinExpr (BLam n t1) = BLam n (\t2 -> joinExpr (t1 t2))
+joinExpr (BLam n t1) = BLam n (joinExpr . t1)
 joinExpr (TApp e t) = TApp (joinExpr e) t
 joinExpr (Lit s) = Lit s
 joinExpr (If e1 e2 e3) = If (joinExpr e1) (joinExpr e2) (joinExpr e3)
@@ -21,11 +21,11 @@ joinExpr (PrimOp e1 o e2) = PrimOp (joinExpr e1) o (joinExpr e2)
 joinExpr (Tuple es) = Tuple (map joinExpr es)
 joinExpr (Proj i e) = Proj i (joinExpr e)
 joinExpr (Fix n1 n2 f t1 t2) = Fix n1 n2 (\e1 e2 -> joinExpr (f (Var n1 e1) (Var n2 e2))) t1 t2
-joinExpr (Let n bind body) = Let n (joinExpr bind) (\e -> joinExpr (body (Var n e)))
+joinExpr (Let n bind body) = Let n (joinExpr bind) (joinExpr . body . Var n)
 joinExpr (LetRec n s b1 b2) =
   LetRec n s
-         (\es -> map joinExpr (b1 (zipWith Var n es)))
-         (\es -> joinExpr (b2 (zipWith Var n es)))
+         (map joinExpr . b1 . zipWith Var n)
+         (joinExpr . b2 . zipWith Var n)
 joinExpr (JNew name es) = JNew name (map joinExpr es)
 joinExpr (JMethod jc m es cn) =
   JMethod (fmap joinExpr jc) m (map joinExpr es) cn
@@ -35,15 +35,15 @@ joinExpr (Seq es) = Seq (map joinExpr es)
 mapExpr :: (Expr t e -> Expr t e) -> Expr t e -> Expr t e
 mapExpr f e =
     case e of
-      Var _ x -> e
-      Lit l -> e
+      Var _ _ -> e
+      Lit _ -> e
       Lam n t g -> Lam n t (\x -> f . g $ x)
       Fix n1 n2 g t1 t -> Fix n1 n2 (\e1 e2 -> f $ g e1 e2) t1 t
       Let n bind body -> Let n (f bind) (\x -> f . body $ x)
       LetRec names sigs binds body ->
           LetRec names sigs
-                 (\es -> map f $ binds es)
-                 (\es -> f $ body es)
+                 (map f . binds)
+                 (f . body)
       BLam n g -> BLam n (\x -> f . g $ x)
       App e1 e2 -> App (f e1) (f e2)
       TApp e t -> TApp (f e) t
@@ -58,7 +58,7 @@ mapExpr f e =
 
 sf2core :: String -> IO (Expr t e)
 sf2core fname = do
-     path <- {-"/Users/weixin/Project/systemfcompiler/compiler/"-} getCurrentDirectory
+     path <- getCurrentDirectory
      string <- readFile (path ++ "/" ++ fname)
      result <- typeCheck . reader $ string
      case result of
@@ -66,4 +66,5 @@ sf2core fname = do
        Right (_, tcheckedSrc) ->
              return . simplify . desugar $ tcheckedSrc
 
-fCore f str = sf2core str >>= (\e -> return $ f e)
+fCore :: (Expr t e -> b) -> String -> IO b
+fCore f str = sf2core str >>= (return . f)
