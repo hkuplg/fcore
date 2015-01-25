4b61768f3b54c7b31b976d19a14b1bdf021630c0
Jeremy Bi <bixuanxbi@gmail.com>
2015-01-25 10:56:10 +0800

    * Comment out `calc` * OptiUtils.hs: checked by hlint

--- compiler/PartialEvaluator.hs
+++ compiler/PartialEvaluator.hs
@@ -20,54 +20,6 @@ app2let (App e1 e2) = case e1' of
     e2' = app2let e2
 app2let e = mapExpr app2let e
 
-calc :: Expr t (Expr t e) -> Expr t (Expr t e)
-calc (App e1 e2) =
-    case (e1', e2') of
-      (Lam _ _ f, Lit _) -> calc . f . joinExpr $ e2'
-      _ -> App e1' e2'
-    where e1' = calc e1
-          e2' = calc e2
-calc (Let n bind body) =
-    case bind' of
-      Lit _ -> calc . body . joinExpr $ bind'
-      _ -> Let n bind' (\x -> calc . body $ x)
-     where bind' = calc bind
-calc (PrimOp e1 op e2) =
-    case (e1', e2') of
-      (Lit (S.Int a), Lit (S.Int b)) ->
-          case op of
-            -- arithmetic operations
-            S.Arith J.Sub -> Lit . S.Int $ a - b
-            S.Arith J.Add -> Lit . S.Int $ a + b
-            S.Arith J.Mult -> Lit . S.Int $ a * b
-            S.Arith J.Div -> Lit . S.Int $ a `div` b
-            S.Arith J.Rem -> Lit . S.Int $ a `rem` b
-            -- comparison operations
-            S.Compare J.Equal -> Lit . S.Bool $ a == b
-            S.Compare J.NotEq -> Lit . S.Bool $ a /= b
-            S.Compare J.LThan -> Lit . S.Bool $ a < b
-            S.Compare J.LThanE -> Lit . S.Bool $ a <= b
-            S.Compare J.GThan -> Lit . S.Bool $ a > b
-            S.Compare J.GThanE -> Lit . S.Bool $ a >= b
-            _ -> simplified
-      (Lit (S.Bool a), Lit (S.Bool b)) ->
-          case op of
-            -- logic operations
-            S.Logic J.CAnd -> Lit . S.Bool $ a && b
-            S.Logic J.COr -> Lit . S.Bool $ a || b
-            _ -> simplified
-      _ -> simplified
-    where e1' = calc e1
-          e2' = calc e2
-          simplified = PrimOp e1' op e2'
-calc (If e1 e2 e3) =
-    case e1' of
-      Lit (S.Bool True) -> calc e2
-      Lit (S.Bool False) -> calc e3
-      _ -> If e1' (calc e2) (calc e3)
-    where e1' = calc e1
-calc e = mapExpr calc e
-
 -- Rule 1: let x = y in e                 => [x -> y] e   (where y is a variable)
 rewrite1 :: Expr t e -> Expr t e
 rewrite1 (Let _ (Var _ n) f) = rewrite1 (f n)
@@ -168,3 +120,51 @@ rewrite e = Hide (rewrite3 . rewrite2 . reveal $ e)
 
 rewriteAndEval :: Exp -> Exp
 rewriteAndEval = rewrite . partialEval
+
+-- calc :: Expr t (Expr t e) -> Expr t (Expr t e)
+-- calc (App e1 e2) =
+--     case (e1', e2') of
+--       (Lam _ _ f, Lit _) -> calc . f . joinExpr $ e2'
+--       _ -> App e1' e2'
+--     where e1' = calc e1
+--           e2' = calc e2
+-- calc (Let n bind body) =
+--     case bind' of
+--       Lit _ -> calc . body . joinExpr $ bind'
+--       _ -> Let n bind' (\x -> calc . body $ x)
+--      where bind' = calc bind
+-- calc (PrimOp e1 op e2) =
+--     case (e1', e2') of
+--       (Lit (S.Int a), Lit (S.Int b)) ->
+--           case op of
+--             -- arithmetic operations
+--             S.Arith J.Sub -> Lit . S.Int $ a - b
+--             S.Arith J.Add -> Lit . S.Int $ a + b
+--             S.Arith J.Mult -> Lit . S.Int $ a * b
+--             S.Arith J.Div -> Lit . S.Int $ a `div` b
+--             S.Arith J.Rem -> Lit . S.Int $ a `rem` b
+--             -- comparison operations
+--             S.Compare J.Equal -> Lit . S.Bool $ a == b
+--             S.Compare J.NotEq -> Lit . S.Bool $ a /= b
+--             S.Compare J.LThan -> Lit . S.Bool $ a < b
+--             S.Compare J.LThanE -> Lit . S.Bool $ a <= b
+--             S.Compare J.GThan -> Lit . S.Bool $ a > b
+--             S.Compare J.GThanE -> Lit . S.Bool $ a >= b
+--             _ -> simplified
+--       (Lit (S.Bool a), Lit (S.Bool b)) ->
+--           case op of
+--             -- logic operations
+--             S.Logic J.CAnd -> Lit . S.Bool $ a && b
+--             S.Logic J.COr -> Lit . S.Bool $ a || b
+--             _ -> simplified
+--       _ -> simplified
+--     where e1' = calc e1
+--           e2' = calc e2
+--           simplified = PrimOp e1' op e2'
+-- calc (If e1 e2 e3) =
+--     case e1' of
+--       Lit (S.Bool True) -> calc e2
+--       Lit (S.Bool False) -> calc e3
+--       _ -> If e1' (calc e2) (calc e3)
+--     where e1' = calc e1
+-- calc e = mapExpr calc e
