b0db6cc5c28dfe975a946547b39de5e70f39eec3
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2015-03-07 17:30:04 +0800

    Remove the sample failing test

--- testsuite/Test.hs
+++ testsuite/Test.hs
@@ -11,8 +11,4 @@ tests = testGroup "Tests" [unitTests]
 unitTests = testGroup "Unit tests"
   [ testCase "List comparison (different length)" $
       [1, 2, 3] `compare` [1,2] @?= GT
-
-  -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
   ]
