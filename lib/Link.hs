cdaa9bb6e8ccfa89ec1dff4f7435d71a7ab733ec
Emma Peng <u3502350@connect.hku.hk>
2015-03-15 15:17:03 +0800

    Namespace for data and type declaration

--- lib/Link.hs
+++ lib/Link.hs
@@ -2,13 +2,15 @@ module Link where
 
 import System.IO
 import System.FilePath	(dropExtension)
+import Data.Char
 -- import Data.List.Split	(splitOn)
 
 link :: FilePath -> String -> IO ()
 link file mod = do
   --let list = lines mod
   --let contM = concatenate (take ((length list) - 1) list)
-  contF <- readFile file
+  content <- readFile file
+  let contF = namespace content
   let newFile = (dropExtension file) ++ "c.sf"
   --writeFile newFile ("let\n" ++ contM ++ "in\n")
   writeFile newFile mod
@@ -33,6 +35,26 @@ addByLine (x:xs) name = name ++ "_" ++ x ++ "\nand\n" ++ (addByLine xs name)
 concatenate :: [String] -> String
 concatenate [] = ""
 concatenate (x:xs) = x ++ "\n" ++ concatenate xs
+
+namespace :: String -> String
+namespace x = let ys = lines x in (unlines . replaceDot) ys
+
+replaceDot :: [String] -> [String]
+replaceDot [] = []
+replaceDot (x:xs) = 
+  let func = unwords . map checkDot in func (words x) : replaceDot xs
+
+checkDot :: [Char] -> String
+checkDot [] = []
+checkDot [x] = [x]
+checkDot (x:y:xs) = if x == '.' && isUpper y 
+                       then ('_' : y : checkDot xs) 
+                       else (x : checkDot (y:xs)) 
+
+test :: FilePath -> IO String
+test file = do
+  content <- readFile file
+  return $ namespace content
 
 {-main = do
   putStr "File: "
