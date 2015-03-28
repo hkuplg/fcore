e58ecb63738d2701d8611abf2f3869e71c4aded0
Zhiyuan Shi <zhiyuanshi@users.noreply.github.com>
2015-03-29 01:15:26 +0800

    Decouple sf2java into FrontEnd.source2core and BackEnd.core2java
    Do not `print` a String; use `putStrLn`
    
    De-tab
    
    Add test case for handling Java exceptions (reverted from commit ba71c5fd02d0a30af6b4c48b40eb6584d83ffb44)
    
    Move FileLoad.hs to testsuite/ as it's not part of the library
    
    Remove imports of frontend modules in BackEnd.hs

--- interpreter/Main.hs
+++ interpreter/Main.hs
@@ -2,24 +2,24 @@
 
 module Main where
 
-import System.Console.Haskeline		(runInputT, defaultSettings)
+import System.Console.Haskeline         (runInputT, defaultSettings)
 import System.IO
 import System.Process hiding (runCommand)
-import System.Directory			(doesFileExist)
+import System.Directory                 (doesFileExist)
 
 import Control.Monad.Error
 
-import Data.FileEmbed			(embedFile)
+import Data.FileEmbed                   (embedFile)
 import qualified Data.Map as Map
 
-import Translations
+import BackEnd
 import JavaUtils
-import StringPrefixes			(namespace)
+import StringPrefixes                   (namespace)
 
 import Loop
 import qualified Environment as Env
 import qualified History as Hist
-import FileIO 				(TransMethod (Naive))
+import FileIO                           (TransMethod (Naive))
 import qualified Data.ByteString as B
 import System.Directory (getTemporaryDirectory)
 import System.FilePath ((</>))
@@ -44,22 +44,22 @@ main = do
      hSetBuffering outP LineBuffering
      liftIO printHelp
      runInputT defaultSettings 
-	       (Loop.loop (inP, outP) (0, compileN, [Naive])  
-		          Map.empty Env.empty Hist.empty Hist.empty 0 False False False False 0)
+               (Loop.loop (inP, outP) (0, compileN, [Naive])  
+                          Map.empty Env.empty Hist.empty Hist.empty 0 False False False False 0)
      terminateProcess proch
      
 fileExist :: String -> IO ()
 fileExist name = do
-	exist <- doesFileExist name
-	if (exist) 
-	  then return ()
-	  else fileExist name	
+        exist <- doesFileExist name
+        if (exist) 
+          then return ()
+          else fileExist name   
 
 printFile :: IO ()
 printFile = do 
-	f <- getLine
-	contents <- readFile f
-	putStr contents
+        f <- getLine
+        contents <- readFile f
+        putStr contents
 
 
 
