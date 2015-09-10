8dad792c39b80ef06c5c2ce3842626b622ea94a4
Jeremy Bi <bixuanxbi@gmail.com>
2015-09-10 23:03:17 +0800

    Fill in some cases for new translation

--- lib/BaseTransCFJavaNew.hs
+++ lib/BaseTransCFJavaNew.hs
@@ -35,8 +35,6 @@ instance (:<) (Translate m) (Translate m) where
 
 type InitVars = [J.BlockStmt]
 
--- Closure F to Java
-
 createCUB :: Maybe J.PackageDecl -> [J.TypeDecl] -> J.CompilationUnit
 createCUB package compDef = cu
   where
@@ -79,3 +77,182 @@ data Translate m = T
     , genClosureVar :: Bool -> Int -> TransJavaExp -> m J.Exp
     , createWrap :: String -> Expr (Var, Type Int) -> m (J.CompilationUnit, Type Int)
     }
+
+getNewVarName :: MonadState Int m => Translate m -> m String
+getNewVarName _ = do
+    (n :: Int) <- get
+    put (n + 1)
+    return $ localvarstr ++ show n
+
+assignVar :: Monad m => Translate m -> Type Int -> String -> J.Exp -> m J.BlockStmt
+assignVar this t varId e = do
+    aType <- javaType this t
+    return $ localFinalVar aType (varDecl varId e)
+
+getTupleClassName :: [a] -> String
+getTupleClassName tuple =
+    if lengthOfTuple > 50
+        then panic "The length of tuple is too long (>50)!"
+        else namespace ++ "tuples.Tuple" ++ show lengthOfTuple
+  where
+    lengthOfTuple = length tuple
+
+trans :: (MonadState Int m, selfType :< Translate m) => Base selfType (Translate m)
+trans self =
+    let this = up self
+    in T
+       { translateM = \e ->
+                           case e of
+                               Var _ (i,t) ->
+                                   return ([], var (localvarstr ++ show i), t)
+                               Lit lit ->
+                                   case lit of
+                                       (S.Int i) ->
+                                           return
+                                               ( []
+                                               , Right $ J.Int i
+                                               , JClass "java.lang.Integer")
+                                       (S.UnitLit) ->
+                                           return ([], Right J.Null, Unit)
+                                       (S.String s) ->
+                                           return
+                                               ( []
+                                               , Right $ J.String s
+                                               , JClass "java.lang.String")
+                                       (S.Bool b) ->
+                                           return
+                                               ( []
+                                               , Right $ J.Boolean b
+                                               , JClass "java.lang.Boolean")
+                                       (S.Char c) ->
+                                           return
+                                               ( []
+                                               , Right $ J.Char c
+                                               , JClass "java.lang.Character")
+                               PrimOp e1 op e2 -> do
+                                   (s1,j1,_) <- translateM this e1
+                                   (s2,j2,_) <- translateM this e2
+                                   let j1' = unwrap j1
+                                   let j2' = unwrap j2
+                                   let (jexpr,typ) =
+                                           case op of
+                                               (S.Arith realOp) ->
+                                                   ( J.BinOp j1' realOp j2'
+                                                   , JClass "java.lang.Integer")
+                                               (S.Compare realOp) ->
+                                                   ( J.BinOp j1' realOp j2'
+                                                   , JClass "java.lang.Boolean")
+                                               (S.Logic realOp) ->
+                                                   ( J.BinOp j1' realOp j2'
+                                                   , JClass "java.lang.Boolean")
+                                   newVarName <- getNewVarName this
+                                   assignExpr <-
+                                       assignVar this typ newVarName jexpr
+                                   return
+                                       ( s1 ++ s2 ++ [assignExpr]
+                                       , var newVarName
+                                       , typ)
+                               If e1 e2 e3 ->
+                                   translateIf
+                                       this
+                                       (translateM this e1)
+                                       (translateM this e2)
+                                       (translateM this e3)
+                               Tuple tuple ->
+                                   case tuple of
+                                       [t] -> do
+                                           (s1,j1,t1) <- translateM this t
+                                           return (s1, j1, TupleType [t1])
+                                       _ -> do
+                                           tuple' <-
+                                               mapM (translateM this) tuple
+                                           let (statements,exprs,types) =
+                                                   (unzip3 tuple') & _1 %~
+                                                   concat
+                                           newVarName <- getNewVarName this
+                                           let c = getTupleClassName tuple
+                                           let rhs =
+                                                   instCreat
+                                                       (classTyp c)
+                                                       (map unwrap exprs)
+                                           assignExpr <-
+                                               assignVar
+                                                   this
+                                                   (JClass c)
+                                                   newVarName
+                                                   rhs
+                                           return
+                                               ( statements ++ [assignExpr]
+                                               , var newVarName
+                                               , TupleType types)
+                               Proj index expr -> do
+                                   ret@(statement,javaExpr,exprType) <-
+                                       translateM this expr
+                                   case exprType of
+                                       TupleType [_] -> return ret
+                                       TupleType types -> do
+                                           newVarName <- getNewVarName this
+                                           let typ = types !! (index - 1)
+                                           aType <- javaType this typ
+                                           let rhs =
+                                                   cast
+                                                       aType
+                                                       (fieldAccess
+                                                            (unwrap javaExpr)
+                                                            ("_" ++ show index))
+                                           assignExpr <-
+                                               assignVar
+                                                   this
+                                                   typ
+                                                   newVarName
+                                                   rhs
+                                           return
+                                               ( statement ++ [assignExpr]
+                                               , var newVarName
+                                               , typ)
+                                       _ ->
+                                           panic
+                                               "BaseTransCFJava.trans: expected tuple type"
+                               App e1 e2 ->
+                                   translateApply
+                                       this
+                                       False
+                                       (translateM this e1)
+                                       (translateM this e2)
+                               Let _ expr body -> do
+                                   (s1,j1,t1) <- translateM this expr
+                                   translateLet this (s1, j1, t1) body
+                               Lam _ se -> do
+                                   (s,je,t) <- translateScopeM this se Nothing
+                                   return (s, je, Pi "_" t)
+                               Mu _ se -> undefined -- TODO: Mu thing
+                               SeqExprs es -> do
+                                   es' <- mapM (translateM this) es
+                                   let (_,lastExp,lastType) = last es'
+                                   let statements =
+                                           concatMap
+                                               (\(x,_,_) ->
+                                                     x)
+                                               es'
+                                   return (statements, lastExp, lastType)
+                               -- Pi, TupleType, Unit, Star, Jnew, JMethod, JField, JClass, Error
+                               _ ->
+                                   panic
+                                       "BaseTransCFJavaNew.trans: don't know how to do"
+       , translateScopeM = undefined
+       , translateApply = undefined
+       , translateIf = undefined
+       , translateLet = undefined
+       , translateScopeTyp = undefined
+       , genApply = undefined
+       , genRes = undefined
+       , applyRetType = undefined
+       , genClone = undefined
+       , withApply = undefined
+       , getPrefix = undefined
+       , javaType = undefined
+       , chooseCastBox = undefined
+       , stackMainBody = undefined
+       , genClosureVar = undefined
+       , createWrap = undefined
+       }
